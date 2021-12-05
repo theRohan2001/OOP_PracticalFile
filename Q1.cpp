41f08174b618b08d5c1949e43760e5614f095f80
Rohan Wadhwa <rohanwadhwa2001@gmail.com>
2021-12-05 14:52:03 +0530

    modified

--- Q1.cpp
+++ Q1.cpp
@@ -4,6 +4,20 @@ using namespace std;
 
 int main(void)
 {
-    cout << "Hello World!";
+    cout << "Enter three numbers: ";
+    int num, num2, num3;
+    cin >> num;
+    cin >> num2;
+    cin >> num3;
+
+    if(num > num2 && num > num3){
+        cout << num << " is the largest number.";
+    }
+    else if(num2 > num && num2 > num3){
+        cout << num2 << " is the largest number.";
+    }
+    else {
+        cout << num3 << " is the largest number.";
+    }
     return 0;
 }
